b7c9400abfe56ba98fa45709b0f9f7e3d97b3fe4
NovaStarmax <lapvpgames@gmail.com>
2024-04-03 10:07:54 +0200

    main

---
+++ dungeonC/main.py
@@ -0,0 +1,6 @@
+# Importations des modules spécifiques du jeu
+from game_functions import start_game, main_menu
+
+# Ici, on lance le menu principal du jeu.
+if __name__ == "__main__":
+    main_menu()
